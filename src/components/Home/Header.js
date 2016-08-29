74bd75f051dd3027214d428fd6ada9ba7404d3fe
PakkuDon <pakku.pd@gmail.com>
2016-08-29 23:55:53 +1000

    Fixed header text and banner placement
    - Banner image scales automatically to container width
    - Petition counter and button now placed below banner on
      smaller screens

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 require('odometer');
 require('odometer/themes/odometer-theme-minimal.css');
 var logo = require('../../images/AFD-logo-text-02.jpg');
+var bannerImg = require('../../images/mike-baird-image.jpg');
 
 class Header extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class Header extends React.Component {
       <div>
         <div className='banner'>
           <img className='logo' src={logo} alt='Australia for Dolphins logo' />
+          <img src={bannerImg} style={{ width: '100%', height: 'auto' }} alt='Petition banner' />
           <div className='header-content'>
             <div className='signature-count center-block'><span className='odometer'>{this.props.count}</span> videos sent to Mike Baird</div>
             <button className='record-btn center-block' onClick={this.props.onAdd}>Create your video now</button>
